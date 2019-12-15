47d835c9990ab4c31e8b23c86d45b0a7b3945858
anthony <agedeon136@gmail.com>
2019-12-15 18:58:16 +0100

    Modification du onBindViewHolder et ajout d'un onItemClick

--- app/src/main/java/com/example/chxbinapp/View/ListContentFragHome.java
+++ app/src/main/java/com/example/chxbinapp/View/ListContentFragHome.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.chxbinapp.Model.AllSport;
@@ -44,7 +45,7 @@ public class ListContentFragHome extends Fragment {
 
         RecyclerView recyclerView = view.findViewById(R.id.my_recycler_view);
 
-        ContentAdapter adapter = new ContentAdapter(getContext(), dataFromA);
+        ContentAdapter adapter = new ContentAdapter(getContext(), dataFromA, listener());
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         // Set padding for Tiles (not needed for Cards/Lists!)
@@ -69,56 +70,67 @@ public class ListContentFragHome extends Fragment {
 
     }
 
-        public class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
-            Context context;
-            List<AllSport> datafromA;
+    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+        Context context;
+        List<AllSport> datafromA;
+        private ContentAdapter.OnItemClickListener listener = null;
 
-
-            public ContentAdapter(Context context, List<AllSport> input) {
-                this.datafromA = input;
-                Resources resources = context.getResources();
-            }
-
-            @Override
-            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-                LayoutInflater inflater = LayoutInflater.from(
-                        parent.getContext());
-                View v =
-                        inflater.inflate(R.layout.row_layout, parent, false);
-
-                ViewHolder vh = new ViewHolder(v);
-                return vh;            }
-
-            @Override
-            public void onBindViewHolder(ViewHolder holder, final int position) {
-                final AllSport currentAll = datafromA.get(position);
-                final String name = currentAll.getStrSport();
-
-                final int obs = currentAll.getIdSport();
-                holder.txtHeader.setText(name);
-                holder.txtFooter.setText("Id: " + obs);
-            }
-
-            @Override
-            public int getItemCount() {
-                return datafromA.size();
-            }
+        public interface OnItemClickListener {
+            void onItemClick(AllSport item);
         }
 
-    public void showListe(List<AllSport> input) {
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(layoutManager);
-        mAdapter = new MyAdapter(input, new MyAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(AllSport item) {
-                Intent intent;
-                intent = new Intent(ListContentFragHome.this.getActivity(), Description.class);
-                intent.putExtra("nom",item.getStrSport());
-                intent.putExtra("description", item.getStrSportDescription());
-                ListContentFragHome.this.startActivity(intent);
-            }
-        });
-        recyclerView.setAdapter(mAdapter);
-    }
+
+        public ContentAdapter(Context context, List<AllSport> input, OnItemClickListener listener) {
+            this.datafromA = input;
+            Resources resources = context.getResources();
+            this.listener = this.listener;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(
+                    parent.getContext());
+            View v =
+                    inflater.inflate(R.layout.row_layout, parent, false);
+
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, final int position) {
+            final AllSport currentAll = datafromA.get(position);
+            final String name = currentAll.getStrSport();
+
+            final int obs = currentAll.getIdSport();
+            holder.txtHeader.setText(name);
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    listener.onItemClick(currentAll);
+                }
+            });
+            holder.txtFooter.setText("Id: " + obs);
+        }
+
+        @Override
+        public int getItemCount() {
+            return datafromA.size();
+        }
     }
 
+
+    private ContentAdapter.OnItemClickListener listener() {
+        return new ContentAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(AllSport item) {
+                Intent intent = new Intent(getContext(), Description.class);
+                Gson gson = new Gson();
+                intent.putExtra("Nom", item.getStrSport());
+                intent.putExtra("description", item.getStrSportDescription());
+                startActivity(intent);
+            }
+
+        };
+    }
+}
